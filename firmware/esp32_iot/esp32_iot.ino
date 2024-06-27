44722efe60997c9b9f2a8426974706cd7eb1d206
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-06-27 17:28:13 -0600

    first version of esp32 firmware

--- firmware/esp32_iot/esp32_iot.ino
+++ firmware/esp32_iot/esp32_iot.ino
@@ -1,108 +1,277 @@
-/*
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#define THINGSBOARD_ENABLE_PROGMEM 0
+#elif defined(ESP32) || defined(RASPBERRYPI_PICO) || defined(RASPBERRYPI_PICO_W)
+#include <WiFi.h>
+#endif
 
- Arduino TFT Bitmap Logo example
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 99
+#endif
 
- This example reads an image file from a micro-SD card
- and draws it on the screen, at random locations.
+#include <Arduino_MQTT_Client.h>
+#include <ThingsBoard.h>
 
- In this sketch, the Arduino logo is read from a micro-SD card.
- There is a .bmp file included with this sketch.
- - open the sketch folder (Ctrl-K or Cmd-K)
- - copy the "arduino.bmp" file to a micro-SD
- - put the SD into the SD slot of the Arduino TFT module.
+constexpr char WIFI_SSID[] = "Labo201";
+constexpr char WIFI_PASSWORD[] = "labo201!";
 
- This example code is in the public domain.
+// See https://thingsboard.io/docs/getting-started-guides/helloworld/
+// to understand how to obtain an access token
+constexpr char TOKEN[] = "b3fdbnpuz6zxc9fm9z3k";
 
- Created 19 April 2013 by Enrico Gueli
+// Thingsboard we want to establish a connection too
+constexpr char THINGSBOARD_SERVER[] = "iot.eie.ucr.ac.cr";
+// MQTT port used to communicate with the server, 1883 is the default unencrypted MQTT port.
+constexpr uint16_t THINGSBOARD_PORT = 1883U;
 
- http://www.arduino.cc/en/Tutorial/TFTBitmapLogo
+// Maximum size packets will ever be sent or received by the underlying MQTT client,
+// if the size is to small messages might not be sent or received messages will be discarded
+constexpr uint32_t MAX_MESSAGE_SIZE = 1024U;
 
- */
+// Baud rate for the debugging serial connection.
+// If the Serial output is mangled, ensure to change the monitor speed accordingly to this variable
+constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
 
-// include the necessary libraries
-#include <SPI.h>
-#include <SD.h>
-#include <TFT.h>  // Arduino LCD library
 
-// pin definition for the Uno
-#define sd_cs  4
-#define lcd_cs 10
-#define dc     9
-#define rst    8
+// Initialize underlying client, used to establish a connection
+WiFiClient wifiClient;
+// Initalize the Mqtt client instance
+Arduino_MQTT_Client mqttClient(wifiClient);
+// Initialize ThingsBoard instance with the maximum needed buffer size
+ThingsBoard tb(mqttClient, MAX_MESSAGE_SIZE);
 
-// pin definition for the Leonardo
-//#define sd_cs  8
-//#define lcd_cs 7
-//#define dc     0
-//#define rst    1
+// Attribute names for attribute request and attribute updates functionality
 
-TFT TFTscreen = TFT(lcd_cs, dc, rst);
+constexpr char BLINKING_INTERVAL_ATTR[] = "blinkingInterval";
+constexpr char LED_MODE_ATTR[] = "ledMode";
+constexpr char LED_STATE_ATTR[] = "ledState";
 
-// this variable represents the image to be drawn on screen
-PImage logo;
+// handle led state and mode changes
+volatile bool attributesChanged = false;
 
+// LED modes: 0 - continious state, 1 - blinking
+volatile int ledMode = 0;
+
+// Current led state
+volatile bool ledState = false;
+
+// Settings for interval in blinking mode
+constexpr uint16_t BLINKING_INTERVAL_MS_MIN = 10U;
+constexpr uint16_t BLINKING_INTERVAL_MS_MAX = 60000U;
+volatile uint16_t blinkingInterval = 1000U;
+
+uint32_t previousStateChange;
+
+// For telemetry
+constexpr int16_t telemetrySendInterval = 2000U;
+uint32_t previousDataSend;
+
+// List of shared attributes for subscribing to their updates
+constexpr std::array<const char *, 2U> SHARED_ATTRIBUTES_LIST = {
+  LED_STATE_ATTR,
+  BLINKING_INTERVAL_ATTR
+};
+
+// List of client attributes for requesting them (Using to initialize device states)
+constexpr std::array<const char *, 1U> CLIENT_ATTRIBUTES_LIST = {
+  LED_MODE_ATTR
+};
+
+/// @brief Initalizes WiFi connection,
+// will endlessly delay until a connection has been successfully established
+void InitWiFi() {
+  Serial.println("Connecting to AP ...");
+  // Attempting to establish a connection to the given WiFi network
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    // Delay 500ms until a connection has been succesfully established
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to AP");
+}
+
+/// @brief Reconnects the WiFi uses InitWiFi if the connection has been removed
+/// @return Returns true as soon as a connection has been established again
+const bool reconnect() {
+  // Check to ensure we aren't connected yet
+  const wl_status_t status = WiFi.status();
+  if (status == WL_CONNECTED) {
+    return true;
+  }
+
+  // If we aren't establish a new connection to the given WiFi network
+  InitWiFi();
+  return true;
+}
+
+
+/// @brief Processes function for RPC call "setLedMode"
+/// RPC_Data is a JSON variant, that can be queried using operator[]
+/// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
+/// @param data Data containing the rpc data that was called and its current value
+/// @return Response that should be sent to the cloud. Useful for getMethods
+RPC_Response processSetLedMode(const RPC_Data &data) {
+  Serial.println("Received the set led state RPC method");
+
+  // Process data
+  int new_mode = data;
+
+  Serial.print("Mode to change: ");
+  Serial.println(new_mode);
+
+  if (new_mode != 0 && new_mode != 1) {
+    return RPC_Response("error", "Unknown mode!");
+  }
+
+  ledMode = new_mode;
+
+  attributesChanged = true;
+
+  // Returning current mode
+  return RPC_Response("newMode", (int)ledMode);
+}
+
+
+// Optional, keep subscribed shared attributes empty instead,
+// and the callback will be called for every shared attribute changed on the device,
+// instead of only the one that were entered instead
+const std::array<RPC_Callback, 1U> callbacks = {
+  RPC_Callback{ "setLedMode", processSetLedMode }
+};
+
+
+/// @brief Update callback that will be called as soon as one of the provided shared attributes changes value,
+/// if none are provided we subscribe to any shared attribute change instead
+/// @param data Data containing the shared attributes that were changed and their current value
+void processSharedAttributes(const Shared_Attribute_Data &data) {
+  for (auto it = data.begin(); it != data.end(); ++it) {
+    if (strcmp(it->key().c_str(), BLINKING_INTERVAL_ATTR) == 0) {
+      const uint16_t new_interval = it->value().as<uint16_t>();
+      if (new_interval >= BLINKING_INTERVAL_MS_MIN && new_interval <= BLINKING_INTERVAL_MS_MAX) {
+        blinkingInterval = new_interval;
+        Serial.print("Blinking interval is set to: ");
+        Serial.println(new_interval);
+      }
+    } else if (strcmp(it->key().c_str(), LED_STATE_ATTR) == 0) {
+      ledState = it->value().as<bool>();
+      if (LED_BUILTIN != 99) {
+        digitalWrite(LED_BUILTIN, ledState);
+      }
+      Serial.print("LED state is set to: ");
+      Serial.println(ledState);
+    }
+  }
+  attributesChanged = true;
+}
+
+void processClientAttributes(const Shared_Attribute_Data &data) {
+  for (auto it = data.begin(); it != data.end(); ++it) {
+    if (strcmp(it->key().c_str(), LED_MODE_ATTR) == 0) {
+      const uint16_t new_mode = it->value().as<uint16_t>();
+      ledMode = new_mode;
+    }
+  }
+}
+
+const Shared_Attribute_Callback attributes_callback(&processSharedAttributes, SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend());
+const Attribute_Request_Callback attribute_shared_request_callback(&processSharedAttributes, SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend());
+const Attribute_Request_Callback attribute_client_request_callback(&processClientAttributes, CLIENT_ATTRIBUTES_LIST.cbegin(), CLIENT_ATTRIBUTES_LIST.cend());
 
 void setup() {
-  // initialize the GLCD and show a message
-  // asking the user to open the serial line
-  TFTscreen.begin();
-  TFTscreen.background(255, 255, 255);
-
-  TFTscreen.stroke(0, 0, 255);
-  TFTscreen.println();
-  TFTscreen.println(F("Arduino TFT Bitmap Example"));
-  TFTscreen.stroke(0, 0, 0);
-  TFTscreen.println(F("Open serial monitor"));
-  TFTscreen.println(F("to run the sketch"));
-
-  // initialize the serial port: it will be used to
-  // print some diagnostic info
-  Serial.begin(9600);
-  while (!Serial) {
-    // wait for serial port to connect. Needed for native USB port only
+  // Initalize serial connection for debugging
+  Serial.begin(SERIAL_DEBUG_BAUD);
+  if (LED_BUILTIN != 99) {
+    pinMode(LED_BUILTIN, OUTPUT);
   }
-
-  // clear the GLCD screen before starting
-  TFTscreen.background(255, 255, 255);
-
-  // try to access the SD card. If that fails (e.g.
-  // no card present), the setup process will stop.
-  Serial.print(F("Initializing SD card..."));
-  if (!SD.begin(sd_cs)) {
-    Serial.println(F("failed!"));
-    return;
-  }
-  Serial.println(F("OK!"));
-
-  // initialize and clear the GLCD screen
-  TFTscreen.begin();
-  TFTscreen.background(255, 255, 255);
-
-  // now that the SD card can be access, try to load the
-  // image file.
-  logo = TFTscreen.loadImage("arduino.bmp");
-  if (!logo.isValid()) {
-    Serial.println(F("error while loading arduino.bmp"));
-  }
+  delay(1000);
+  InitWiFi();
 }
 
 void loop() {
-  // don't do anything if the image wasn't loaded correctly.
-  if (logo.isValid() == false) {
+  delay(10);
+
+  if (!reconnect()) {
     return;
   }
 
-  Serial.println(F("drawing image"));
+  if (!tb.connected()) {
+    // Connect to the ThingsBoard
+    Serial.print("Connecting to: ");
+    Serial.print(THINGSBOARD_SERVER);
+    Serial.print(" with token ");
+    Serial.println(TOKEN);
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN, THINGSBOARD_PORT)) {
+      Serial.println("Failed to connect");
+      return;
+    }
+    // Sending a MAC address as an attribute
+    tb.sendAttributeData("macAddress", WiFi.macAddress().c_str());
 
-  // get a random location where to draw the image.
-  // To avoid the image to be draw outside the screen,
-  // take into account the image size.
-  int x = random(TFTscreen.width() - logo.width());
-  int y = random(TFTscreen.height() - logo.height());
+    Serial.println("Subscribing for RPC...");
+    // Perform a subscription. All consequent data processing will happen in
+    // processSetLedState() and processSetLedMode() functions,
+    // as denoted by callbacks array.
+    if (!tb.RPC_Subscribe(callbacks.cbegin(), callbacks.cend())) {
+      Serial.println("Failed to subscribe for RPC");
+      return;
+    }
 
-  // draw the image to the screen
-  TFTscreen.image(logo, x, y);
+    if (!tb.Shared_Attributes_Subscribe(attributes_callback)) {
+      Serial.println("Failed to subscribe for shared attribute updates");
+      return;
+    }
 
-  // wait a little bit before drawing again
-  delay(1500);
+    Serial.println("Subscribe done");
+
+    // Request current states of shared attributes
+    if (!tb.Shared_Attributes_Request(attribute_shared_request_callback)) {
+      Serial.println("Failed to request for shared attributes");
+      return;
+    }
+
+    // Request current states of client attributes
+    if (!tb.Client_Attributes_Request(attribute_client_request_callback)) {
+      Serial.println("Failed to request for client attributes");
+      return;
+    }
+  }
+
+  if (attributesChanged) {
+    attributesChanged = false;
+    if (ledMode == 0) {
+      previousStateChange = millis();
+    }
+    tb.sendTelemetryData(LED_MODE_ATTR, ledMode);
+    tb.sendTelemetryData(LED_STATE_ATTR, ledState);
+    tb.sendAttributeData(LED_MODE_ATTR, ledMode);
+    tb.sendAttributeData(LED_STATE_ATTR, ledState);
+  }
+
+  if (ledMode == 1 && millis() - previousStateChange > blinkingInterval) {
+    previousStateChange = millis();
+    ledState = !ledState;
+    tb.sendTelemetryData(LED_STATE_ATTR, ledState);
+    tb.sendAttributeData(LED_STATE_ATTR, ledState);
+    if (LED_BUILTIN == 99) {
+      Serial.print("LED state changed to: ");
+      Serial.println(ledState);
+    } else {
+      digitalWrite(LED_BUILTIN, ledState);
+    }
+  }
+
+  // Sending telemetry every telemetrySendInterval time
+  if (millis() - previousDataSend > telemetrySendInterval) {
+    previousDataSend = millis();
+    tb.sendTelemetryData("temperature", random(10, 20));
+    tb.sendAttributeData("rssi", WiFi.RSSI());
+    tb.sendAttributeData("channel", WiFi.channel());
+    tb.sendAttributeData("bssid", WiFi.BSSIDstr().c_str());
+    tb.sendAttributeData("localIp", WiFi.localIP().toString().c_str());
+    tb.sendAttributeData("ssid", WiFi.SSID().c_str());
+  }
+
+  tb.loop();
 }
+
